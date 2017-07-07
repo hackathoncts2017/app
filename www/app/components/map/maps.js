1de4aab0bf64b98478309c66ca9a9d21b6687852
Vignesh Laxman <Vigneshlaxman91@gmail.com>
2017-07-08 01:21:06 +0530

    Map changes

--- www/app/components/map/maps.js
+++ www/app/components/map/maps.js
@@ -1,13 +1,20 @@
 hackathon.controller("MapController", function(shared, $state, $scope, $mdSidenav, $mdComponentRegistry,NgMap,$rootScope) {
 	NgMap.getMap().then(function(map) {
-	    console.log(map.getCenter());
-	    console.log('markers', map.markers);
-	    console.log('shapes', map.shapes);
+		$rootScope.mapDirections = map.directionsRenderers[0];
 	  });
 
 	$rootScope.$on("MapSpeech", function(controller,data){
            console.log(data)
     });
+    $rootScope.$on("headedText", function(controller,data){
+    	if(data.header == "Map") {
+    		if($rootScope.speeckToUser){
+                 $rootScope.speeckToUser({"text":"Distance to your destination is " + $rootScope.mapDirections.directions.routes[0].legs[0].distance.text + 
+                 	" and total time takes is " + $rootScope.mapDirections.directions.routes[0].legs[0].duration.text})  
+            }
+    	}
+		console.log(data);
+   });
     //$rootScope.$emit("headedText", {"header":"Map"});
 
 })
