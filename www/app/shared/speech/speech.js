059ccc35b7855f8eea7a802c7520370a5a504432
saravanan <sarvon77@hotmail.com>
2017-07-07 22:14:58 +0530

    dsfs

--- www/app/shared/speech/speech.js
+++ www/app/shared/speech/speech.js
@@ -1,13 +1,16 @@
 hackathon.controller("SpeechController", function(shared, $state, $scope, $mdSidenav, $mdComponentRegistry,$rootScope) {
+	
 	$scope.audioinput = function(){
-		$scope.callcomponent();
+		$scope.speechRecognition = window.plugins && window.plugins.speechRecognition ? window.plugins.speechRecognition : false; 
+		//$scope.callcomponent();
+		$scope.speechReg();
 	}
 
 	$scope.callcomponent = function(){
 		//console.log($rootScope.selectedComponent)
 		$rootScope.$emit($rootScope.selectedComponent, {"text":"Hi"});
 	}
-
+	
 	$rootScope.speeckToUser = function(data) {
 		if(typeof TTS != "undefined") {
 			TTS.speak(data.text, function () {
@@ -17,4 +20,49 @@ hackathon.controller("SpeechController", function(shared, $state, $scope, $mdSid
 	        });
     	}
 	}
+	$scope.speechReg = function(){
+		if($scope.speechRecognition) {
+			$scope.speechRecognition.isRecognitionAvailable($scope.regconitaionCb);
+			$scope.regconitaionCb = function(isAvaliable) {
+				if(isAvaliable) {
+					$scope.getPermission();
+				} else {
+					alert("Your device do not have speech recognition");
+				}
+			};
+			
+			$scope.getPermission = function() {
+				$scope.speechRecognition.hasPermission(function(isPermission) {
+					if(isPermission) {
+						$scope.listen();
+					} else {
+						$scope.makePermission();
+					}
+				});
+			}
+			$scope.listen = function() {
+				let options = {
+				  language:"en-US",
+				  matches:1,
+				  prompt:"",
+				  showPopup:true,
+				  showPartial:true
+				}
+				$scope.speechRecognition.startListening($scope.successCb,function(){console.log("ddd")},options)	
+			}
+			$scope.successCb = function(data) {
+				var result= data[0];
+				$rootScope.speeckToUser({"text":result})
+				
+			}
+			$scope.makePermission = function() {
+				$scope.speechRecognition.requestPermission(function(){
+					$scope.listen();
+				}, function error() {
+					alert("You have denyed a permission")
+				})
+			}
+		}
+	}
+	
 });
