6af8cbfacca5472e995ca5db0f3778c0e30b24a8
Johnny2528 <johnny2528@gmail.com>
2017-07-26 11:15:08 +0530

    Update registerDeviceController

--- www/app/components/register/registerDeviceController.js
+++ www/app/components/register/registerDeviceController.js
@@ -2,12 +2,17 @@
 (function(){
   'use strict';
 
-  	angular.module('ngapp').controller('registerDeviceController',['$scope','$rootScope','$timeout',function($scope,$rootScope,$timeout){
+  	angular.module('ngapp').controller('registerDeviceController',['$scope','$timeout',function($scope,$timeout){
+        //$scope.$parent.displayHeaderFooter(false,false);
         $scope.initRegcall = true;
-
+        $scope.isListening = false;
         $scope.showSuccess = false;
         $scope.enableMic = function(){
 
+        }
+
+        $scope.afterHide = function(){
+            alert("Start your successfull registration screen.");
         }
 
         $scope.registerDevice = function(){
@@ -16,17 +21,28 @@
                 var registerNumber =  $("register-field").text();
                 if(isNaN(registerNumber)){
                     alert("Register Number is not Valid");
-                }else if(registerNumber === "24352"){
+                }else if(registerNumber === "23456"){
                     $timeout(function(){
-                        alert("Register Number is not Valid");
                         $scope.showSuccess = true;
                     },1500);
-                }else{
-                    
                 }
             }
         }
-      }]);
+
+        $scope.enableMicSpeech = function(){
+            console.log("Trigerring....");
+            $scope.isListening = !$scope.isListening;
+            $scope.micImg = $scope.isListening?"assets/icons/ic_mic_red.gif":"assets/icons/ic_mic.png";
+        }
+
+        $scope.successMsg = [{header:'Registration Successful',content:'Welcome, You have successfully registed your device with Digital Service Assistance. Now all your daily service calls will be in your finger tips.'},
+        {header:'Why Digital Service Assistance?',content:'Digital service assistance is to enhance the quality of service provided by service agents? You can track and update your daily service calls. Get technical assistance from high technical assistance and get video suggestion from global portal. All your needed products can be raised request from your place itself.'},
+        {header:'How to check all assigned tasks?',content:'Digital service assistance is to enhance the quality of service provided by service agents? You can track and update your daily service calls. Get technical assistance from high technical assistance and get video suggestion from global portal. All your needed products can be raised request from your place itself.'},
+        {header:'How to get technical assistance?',content:'Digital service assistance is to enhance the quality of service provided by service agents? You can track and update your daily service calls. Get technical assistance from high technical assistance and get video suggestion from global portal. All your needed products can be raised request from your place itself.'},
+        {header:'How to place order for new products?',content:'Digital service assistance is to enhance the quality of service provided by service agents? You can track and update your daily service calls. Get technical assistance from high technical assistance and get video suggestion from global portal. All your needed products can be raised request from your place itself.'},
+        {header:'Commands',content:'Digital service assistance is to enhance the quality of service provided by service agents? You can track and update your daily service calls. Get technical assistance from high technical assistance and get video suggestion from global portal. All your needed products can be raised request from your place itself.'},]
+       
+  	}]);
     
       angular.module('ngapp').directive('splitheight', function($compile, $timeout) {
       return {
