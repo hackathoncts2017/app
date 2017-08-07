daacdec6a6e3ab578b877dcbc457ce8c7abd1331
saravanan <sarvon77@hotmail.com>
2017-08-07 23:22:15 +0530

    dd

--- www/app/app.js
+++ www/app/app.js
@@ -9,7 +9,7 @@ hackathon = angular.module("ngapp", [ "ui.router", "ngMaterial", "ngCordova", "n
 	if(typeof device != "undefined" ) {
 		if(localStorage.userToggle == undefined) {
 			//commenting for testing
-			//localStorage.deviceDetails = JSON.stringify(device);		
+			localStorage.deviceDetails = JSON.stringify(device);		
 		}
 		cordova.plugins.diagnostic.isLocationEnabled(function(enabled){
 			console.log("Location is " + (enabled ? "enabled" : "disabled"));
@@ -21,16 +21,16 @@ hackathon = angular.module("ngapp", [ "ui.router", "ngMaterial", "ngCordova", "n
 		});
 	}
   }, false);
-  var devideId = "c0e9928ff73b8fa1";
-  localStorage.deviceDetails = JSON.stringify({"available":true,"platform":"Android","version":"7.0","uuid": devideId ,"cordova":"6.2.3","model":"Lenovo K33a42","manufacturer":"LENOVO","isVirtual":false,"serial":"d4d2cc44"} );        
+ //var devideId = "c0e9928ff73b8fa11";
+  //localStorage.deviceDetails = JSON.stringify({"available":true,"platform":"Android","version":"7.0","uuid": devideId ,"cordova":"6.2.3","model":"Lenovo K33a42","manufacturer":"LENOVO","isVirtual":false,"serial":"d4d2cc44"} );        
 
 if(localStorage.userToggle){
-	var devideId = "c0e9928ff73b8fa1"
+	var devideId = "c0e9928ff73b8fa11"
 	if(localStorage.userToggle  == "admin") {
-		devideId = "c0e9928ff73b8fa1";
+		devideId = "c0e9928ff73b8fa11";
 	} else if(localStorage.userToggle  == "engineer") {
 		devideId = "1234567890";
-	} else {
+	} else  {
 		devideId = "1234509876";
 	}
 	localStorage.deviceDetails = JSON.stringify({"available":true,"platform":"Android","version":"7.0","uuid": devideId ,"cordova":"6.2.3","model":"Lenovo K33a42","manufacturer":"LENOVO","isVirtual":false,"serial":"d4d2cc44"} );        
