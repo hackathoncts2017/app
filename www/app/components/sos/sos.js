13bbe4c52ba3d13b0720e50225e5da03f4b60c43
sarvon77 <sarvon77@hotmail.com>
2018-01-05 16:56:19 +0530

    Add files via upload

---
+++ www/app/components/sos/sos.js
@@ -0,0 +1,10 @@
+
+(function(){
+  'use strict';
+
+  	angular.module('ngapp').controller('sosController',['$scope','$timeout','$rootScope','$window',function($scope,$timeout,$rootScope,$window){
+        
+  	}]);
+ 
+   
+  })();
